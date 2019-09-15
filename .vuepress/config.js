63e05790c6c830f907eec9fa7a832fae67f57b38
Manuel Wieser <office@manuelwieser.com>
2019-09-15 17:41:12 +0200

    Inline SVGs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,5 +2,10 @@ module.exports = {
     title: 'Teresa & Manuel',
     head: [
         ['link', { href: 'https://fonts.googleapis.com/css?family=Abril+Fatface|Lato:light,normal,bold&display=swap', rel: 'stylesheet' }]
-    ]
+    ],
+    chainWebpack: (config) => {
+        config.module.rules.delete('svg');
+        config.module.rule('images')
+            .test(/\.(svg|png|jpe?g|gif|webp)(\?.*)?$/);
+    },
 };
