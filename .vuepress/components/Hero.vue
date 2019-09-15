768ba1b8139adc2ce0c148eb962e3ba9529ae0e2
Manuel Wieser <office@manuelwieser.com>
2019-09-15 10:08:31 +0200

    Adapt hero for all screen sizes

--- .vuepress/components/Hero.vue
+++ .vuepress/components/Hero.vue
@@ -5,41 +5,58 @@
             <img class="circle__decoration circle__decoration--top" src="../images/emblem-blumen-oben.png">
             <img class="circle__decoration circle__decoration--bottom" src="../images/emblem-blumen-unten.png">
         </div>
-        <img class="header__glue" src="../images/blumen-2.png">
     </header>
 </template>
 
 <style lang="scss" scoped>
     .header {
         position: relative;
-        display: flex;
-        align-items: flex-start;
-        justify-content: flex-end;
-        padding: 3rem 9rem;
-        background: url(../images/header-tileable.jpg) center 30%;
-        background-size: auto 135%;
+        background: url(../images/header-tileable.jpg) center;
+        background-size: auto 100%;
+        min-height: 15rem;
 
-        --circle-size: 24rem;
+        @media (min-width: 30rem) {
+            min-height: 20rem;
+        }
+
+        @media (min-width: 45rem) {
+            display: flex;
+            align-items: flex-start;
+            justify-content: flex-end;
+            padding: 3rem 9rem;
+            background-position: center 30%;
+            background-size: auto 135%;
+        }
     }
-    .header__glue {
-        position: absolute;
-        bottom: calc(var(--circle-size) * -0.175);
-        left: 0;
-        width: calc(var(--circle-size) / 2);
-    }
+
     .circle {
-        position: relative;
-        width: var(--circle-size);
-        height: var(--circle-size);
-        max-width: none;
-        max-height: none;
+        display: none;
+
+        @media (min-width: 60rem) {
+            position: relative;
+            display: block;
+
+            --circle-size: 16rem;
+            width: var(--circle-size);
+            height: var(--circle-size);
+        }
+
+        @media (min-width: 70rem) {
+            --circle-size: 20rem;
+        }
+
+        @media (min-width: 80rem) {
+            --circle-size: 24rem;
+        }
     }
+
     .circle__title {
         overflow: hidden;
         width: 100%;
         height: 100%;
         border-radius: 50%;
     }
+
     .circle__decoration {
         position: absolute;
         width: 50%;
@@ -49,6 +66,7 @@
             left: 0;
             transform: translate(0, -35%);
         }
+
         &--bottom {
             bottom: 0;
             right: 0;
